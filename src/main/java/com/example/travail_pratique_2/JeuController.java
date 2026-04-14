84811110631b2acf86b4de8c0a7aa6b982ed33ab
Legraciste <jpleg2626@gmail.com>
2026-04-14 17:38:28 -0400

    Ajout des id et des evenements sur les bouton

--- src/main/java/com/example/travail_pratique_2/JeuController.java
+++ src/main/java/com/example/travail_pratique_2/JeuController.java
@@ -1,4 +1,41 @@
 package com.example.travail_pratique_2;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
 public class JeuController {
+
+    @FXML
+    private GridPane board;
+
+    @FXML
+    private Button boutonLancerDe;
+
+    @FXML
+    private Button boutonRecommencerPartie;
+
+    @FXML
+    private Button boutonRetourAccueil;
+
+    @FXML
+    private Label labelTourDuJoueur;
+
+    @FXML
+    void onBtnLancerDeAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBtnRecommencerPartieAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBtnRetournerAccueilAction(ActionEvent event) {
+
+    }
+
 }
