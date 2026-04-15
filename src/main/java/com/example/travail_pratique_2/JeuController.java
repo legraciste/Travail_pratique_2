56d90f6c565a91712ce930e732c65bd252fed26f
Legraciste <jpleg2626@gmail.com>
2026-04-15 10:28:40 -0400

    Arrangement des faces du de/reajustement du design de la page jeu

--- src/main/java/com/example/travail_pratique_2/JeuController.java
+++ src/main/java/com/example/travail_pratique_2/JeuController.java
@@ -5,8 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.Random;
 
 public class JeuController {
 
@@ -15,6 +18,9 @@ public class JeuController {
 
     @FXML
     private Button boutonLancerDe;
+
+    @FXML
+    private ImageView imageFaceDuDe;
 
     @FXML
     private Button boutonRecommencerPartie;
@@ -27,6 +33,18 @@ public class JeuController {
 
     @FXML
     void onBtnLancerDeAction(ActionEvent event) {
+        int numeroDuLancer = new Random().nextInt(6) + 1; // Simule le lancer de dé (1 à 6)
+        try {
+            String path = "/images/Face_de_de_numero_" + numeroDuLancer + ".png";
+            System.out.println("Tentative de chargement de l'image : " + path);
+            Image image = new Image(Objects.requireNonNull(getClass().getResource(path)).toExternalForm());
+            imageFaceDuDe.setPreserveRatio(true); // Préserver les proportions pour éviter la distorsion
+            imageFaceDuDe.setImage(image); //Affichage de l'image du dé correspondant au lancer
+            System.out.println("Image chargée avec succès pour le numéro : " + numeroDuLancer);
+        } catch (Exception e) {
+            System.err.println("Erreur lors du chargement de l'image pour le numéro " + numeroDuLancer + " : " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @FXML
